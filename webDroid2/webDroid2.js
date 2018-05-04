a6a820dd592dbe3cf652c1a741329eb9d15c4555
duncan-coffee <duncanharrist@Duncans-MacBook-Pro.local>
2018-05-04 12:18:18 -0500

    cool

--- webDroid2/webDroid2.js
+++ webDroid2/webDroid2.js
@@ -1,29 +1,39 @@
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(0);
-  noStroke(); 
+  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent('sketch-holder');
+  background(100,150,200);
+  noStroke();
+
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+
+
 }
 
-var pos = 10; 
+var pos = 10;
 var rotateVar = 0;
-var rotateTemp = 3.14; 
-var fillVar = .1; 
+var rotateTemp = 3.15;
+var fillVar = .1;
 
 function draw() {
-  
+
   translate(width / 2, height / 2);
-  
-  rotate(rotateVar); 
+
+  rotate(rotateVar);
   rotateVar = rotateVar + rotateTemp;
-  
-  fillVar += .01;
+
+  fillVar += .02;
   fill(abs(sin(fillVar/5))*250,
        abs(sin(fillVar/2))*250,
-       abs(sin(fillVar/3))*250);
-       
-   pos = pos + .1; 
+       abs(sin(fillVar/30))*250);
+
+   pos = pos + .25;
 
   ellipse(pos,pos,pos,pos);
-  
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  background(20,10,50);
 }
