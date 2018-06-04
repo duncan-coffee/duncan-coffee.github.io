db57b1e9eaf571ed0105dde02fc44abdc47641c5
duncan-coffee <duncanharrist@Duncans-MacBook-Pro.local>
2018-06-04 13:53:19 -0500

    new files

--- webDroid2/webDroid2.js
+++ webDroid2/webDroid2.js
@@ -9,18 +9,22 @@ function setup() {
 var pos = 10;
 var rotateVar = 0;
 var rotateTemp = 3.15;
-var fillVar = .1;
+var fillVar = 0;
+var fillTemp = .1;
 
 function draw() {
   translate(width / 2, height / 2);
   rotate(rotateVar);
   rotateVar = rotateVar + rotateTemp;
-  fillVar += .02;
+  fillVar = fillVar + fillTemp;
   fill(abs(sin(fillVar/5))*250,
        abs(sin(fillVar/2))*250,
        abs(sin(fillVar/30))*250);
   pos = pos + .25;
   ellipse(pos,pos,pos,pos);
+  fill(100);
+  rect(windowWidth*.33,50);
+  rect(windowWidth*.33,windowHeight-50);
 }
 
 function windowResized() {
